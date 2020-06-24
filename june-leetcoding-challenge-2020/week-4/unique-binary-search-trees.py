eb31321606d036eb9c7734bc21be746947a9a018
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2020-06-25 00:06:31 +0530

    add another approach

--- june-leetcoding-challenge-2020/week-4/unique-binary-search-trees.py
+++ june-leetcoding-challenge-2020/week-4/unique-binary-search-trees.py
@@ -1,9 +1,23 @@
 from math import factorial
 class Solution:
     def numTrees(self, n: int) -> int:
-        return factorial(2*n)//(factorial(n)*factorial(n)*(n+1))
+    	## using Dynamic programming
+        if n==0 or n==1:
+            return 1
+        
+        dp=[0]*(n+1)
+        dp[0]=1
+        dp[1]=1
+        
+        for i in range(2,n+1):
+            for j in range(i):
+                dp[i]+=(dp[j]*dp[i-j-1])
+        
+        return dp[n]
+            
+        
+        
+        
+#        return factorial(2*n)//(factorial(n)*factorial(n)*(n+1))  ## catalan number formula
+        
 
-
-s = Solution()
-n=5
-print(s.numTrees(n))
